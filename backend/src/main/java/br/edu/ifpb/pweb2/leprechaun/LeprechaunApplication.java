140876cc14f6cf9c2f7cd6006f68faf95d7169b7
imbruh <brunaluiza112001@gmail.com>
2021-07-19 11:00:03 -0300

    iniciando projeto do back

---
+++ backend/src/main/java/br/edu/ifpb/pweb2/leprechaun/LeprechaunApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.leprechaun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeprechaunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeprechaunApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/ifpb/pweb2/leprechaun/LeprechaunApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.leprechaun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeprechaunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
