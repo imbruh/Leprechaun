2f336a5dfe83cdc57a86039f08ab2052d3b21656
imbruh <brunaluiza112001@gmail.com>
2021-09-16 15:43:41 -0300

    começando no thymeleaf

--- backend/src/main/java/br/edu/ifpb/pweb2/leprechaun/Controller/UsuarioController.java
+++ backend/src/main/java/br/edu/ifpb/pweb2/leprechaun/Controller/UsuarioController.java
@@ -1,20 +1,24 @@
 package br.edu.ifpb.pweb2.leprechaun.Controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 import br.edu.ifpb.pweb2.leprechaun.Model.Usuario;
 import br.edu.ifpb.pweb2.leprechaun.Repository.UsuarioRepository;
 import br.edu.ifpb.pweb2.leprechaun.Service.UsuarioService;
 
-@RestController
+@Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
     
@@ -38,5 +42,12 @@ public class UsuarioController {
     public ResponseEntity<Usuario> cadastrarCliente(@RequestBody Usuario usuario){
         Usuario user = this.usuarioService.cadastrarUsuario(usuario);
         return new ResponseEntity<> (user, HttpStatus.OK);
+    }
+    
+    @GetMapping("/index")
+    public ModelAndView testando(ModelAndView mav) {
+    	mav.setViewName("index");
+    	mav.addObject("idade", 20);
+    	return mav;
     }
 }
