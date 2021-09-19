93d214db890a13bebdba40d27b91a8da4a2aacce
imbruh <brunaluiza112001@gmail.com>
2021-09-18 21:56:05 -0300

    usando post-redirect-get no login

--- backend/src/main/java/br/edu/ifpb/pweb2/leprechaun/Controller/UsuarioController.java
+++ backend/src/main/java/br/edu/ifpb/pweb2/leprechaun/Controller/UsuarioController.java
@@ -11,9 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -53,32 +56,44 @@ public class UsuarioController {
         Usuario user = this.usuarioService.cadastrarUsuario(usuario);
         return new ResponseEntity<> (user, HttpStatus.OK);
     }
+    
+    @RequestMapping("/index")
+    public String index(Model model) {
+    	model.addAttribute("usuario", new Usuario());
+    	return "index";
+    }
   
-    @PostMapping("/login")
-    public String logarUsuario(@ModelAttribute Usuario usuario, Model model, RedirectAttributes redirectAttributes) {
+    @RequestMapping("/login")
+    public ModelAndView logarUsuario(ModelAndView mav, Usuario usuario) {
     	Usuario usuarioExistente = this.usuarioService.logarUsuario(usuario);
-    	
+  
     	if(usuarioExistente == null) {
-    		redirectAttributes.addFlashAttribute("Erro", "Login ou senha inválidos");
-    		return "redirect:/login";
+//    		redirectAttributes.addFlashAttribute("Erro", "Login ou senha inválidos");
+    		mav.addObject("mensagem", "Login ou senha invalidos");
+    		mav.setViewName("index");
+    		return mav;
     	}
     	
-    	redirectAttributes.addFlashAttribute("usuario", usuario);
-        return "redirect:/usuarios/sorteio";
+    	mav.addObject("usuario", usuarioExistente);
+    	mav.setViewName("redirect:/usuario/sorteio/" + usuarioExistente.getId());
+    	
+//    	redirectAttributes.addFlashAttribute("usuario", usuario);
+        return mav;
     }
     
-
-    @GetMapping("/sorteio")
-    public String testando(@ModelAttribute Usuario usuario, ModelAndView mav) {
-    	Sorteio sorteio = this.sorteioService.getSorteioAberto();
-    	mav.setViewName("Telas/TelaInicialSorteio");
-    	mav.addObject("sorteio", sorteio);
+    
+    @RequestMapping("/sorteio/{idUsuario}")
+    public ModelAndView getTelaSorteio(ModelAndView mav, @PathVariable Long idUsuario) {
+    	Usuario usuario = this.usuarioRepository.findById(idUsuario).orElse(null);
+    	
     	mav.addObject("usuario", usuario);
-    	System.out.println(usuario);
+    	mav.setViewName("Telas/TelaInicialSorteio");
+    	Sorteio sorteio = this.sorteioService.getSorteioAberto();
+    	mav.addObject("sorteio", sorteio);
     	if(sorteio != null) {
     		mav.addObject("dataFormatada", sorteio.getDataHora().format(DateTimeFormatter.ofPattern("dd/MM/yyy")));
     	}
     	
-    	return "/usuarios/sorteio";
+    	return mav;
     }
 }
